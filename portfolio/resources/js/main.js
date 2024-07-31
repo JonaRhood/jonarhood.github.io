c4d312462bfc02da6f56c0b63afa731a2933aa1a
Jonathan Cano <jonathancanofreta@gmail.com>
2024-07-31 11:01:51 +0200

    Updates on main.js and scrolldown logo

--- portfolio/resources/js/main.js
+++ portfolio/resources/js/main.js
@@ -2,7 +2,7 @@
 // Assignations
 const bannerExp = ['bannerexp1', 'bannerexp2', 'bannerexp3', 'bannerexp4', 'bannerexp5'];
 const banners = [];
-bannerExp.forEach(function(element) {
+bannerExp.forEach(function (element) {
     banners.push(document.getElementById(element));
 });
 
@@ -40,91 +40,97 @@ const addToClickedBanner = banner => {
     console.log(selectedBanner);
 
     //quit color from old banner
-       unclickedColor(clickedBanner);
-       
+    unclickedColor(clickedBanner);
+
 
     // sort the numbers and apply the changes
     for (let i = 0; i < selectedBanner.length; i++) {
-       if (selectedBanner[i].includes('1')) {
-        clickedBanner = banners[0];
-        clickedColor(clickedBanner);
-        clickedMinus('signbanner1');
-        unclickedMinus('signbanner2');
-        unclickedMinus('signbanner3');
-        unclickedMinus('signbanner4');
-        unclickedMinus('signbanner5');
-        // show smoothly and hide others
-        document.getElementById('hiddenbanner1').style.display = 'flex';
-        document.getElementById('hiddenbanner2').style.display = 'none';
-        document.getElementById('hiddenbanner3').style.display = 'none';
-        document.getElementById('hiddenbanner4').style.display = 'none';
-        document.getElementById('hiddenbanner5').style.display = 'none';
-       }  else if (selectedBanner[i].includes('2')) {
-        clickedBanner = banners[1];
-        clickedColor(clickedBanner);
-        clickedMinus('signbanner2');
-        unclickedMinus('signbanner1');
-        unclickedMinus('signbanner3');
-        unclickedMinus('signbanner4');
-        unclickedMinus('signbanner5');
-        document.getElementById('hiddenbanner2').style.display = 'flex';
-        document.getElementById('hiddenbanner1').style.display = 'none';
-        document.getElementById('hiddenbanner3').style.display = 'none';
-        document.getElementById('hiddenbanner4').style.display = 'none';
-        document.getElementById('hiddenbanner5').style.display = 'none';
-       }  else if (selectedBanner[i].includes('3')) {
-        clickedBanner = banners[2];
-        clickedColor(clickedBanner);
-        clickedMinus('signbanner3');
-        unclickedMinus('signbanner1');
-        unclickedMinus('signbanner2');
-        unclickedMinus('signbanner4');
-        unclickedMinus('signbanner5');
-        document.getElementById('hiddenbanner3').style.display = 'flex';
-        document.getElementById('hiddenbanner1').style.display = 'none';
-        document.getElementById('hiddenbanner2').style.display = 'none';
-        document.getElementById('hiddenbanner4').style.display = 'none';
-        document.getElementById('hiddenbanner5').style.display = 'none';
-       }  else if (selectedBanner[i].includes('4')) {
-        clickedBanner = banners[3];
-        clickedColor(clickedBanner);
-        clickedMinus('signbanner4');
-        unclickedMinus('signbanner1');
-        unclickedMinus('signbanner2');
-        unclickedMinus('signbanner3');
-        unclickedMinus('signbanner5');
-        document.getElementById('hiddenbanner4').style.display = 'flex';
-        document.getElementById('hiddenbanner1').style.display = 'none';
-        document.getElementById('hiddenbanner2').style.display = 'none';
-        document.getElementById('hiddenbanner3').style.display = 'none';
-        document.getElementById('hiddenbanner5').style.display = 'none';
-       }  else if (selectedBanner[i].includes('5')) {
-        clickedBanner = banners[4];
-        clickedColor(clickedBanner);
-        clickedMinus('signbanner5');
-        unclickedMinus('signbanner1');
-        unclickedMinus('signbanner2');
-        unclickedMinus('signbanner3');
-        unclickedMinus('signbanner4');
-        document.getElementById('hiddenbanner5').style.display = 'flex';
-        document.getElementById('hiddenbanner1').style.display = 'none';
-        document.getElementById('hiddenbanner2').style.display = 'none';
-        document.getElementById('hiddenbanner3').style.display = 'none';
-        document.getElementById('hiddenbanner4').style.display = 'none';
-       }
+        if (selectedBanner[i].includes('1')) {
+            clickedBanner = banners[0];
+            clickedColor(clickedBanner);
+            clickedMinus('signbanner1');
+            unclickedMinus('signbanner2');
+            unclickedMinus('signbanner3');
+            unclickedMinus('signbanner4');
+            unclickedMinus('signbanner5');
+            // show smoothly and hide others
+            document.getElementById('hiddenbanner1').style.display = 'flex';
+            document.getElementById('hiddenbanner2').style.display = 'none';
+            document.getElementById('hiddenbanner3').style.display = 'none';
+            document.getElementById('hiddenbanner4').style.display = 'none';
+            document.getElementById('hiddenbanner5').style.display = 'none';
+        } else if (selectedBanner[i].includes('2')) {
+            clickedBanner = banners[1];
+            clickedColor(clickedBanner);
+            clickedMinus('signbanner2');
+            unclickedMinus('signbanner1');
+            unclickedMinus('signbanner3');
+            unclickedMinus('signbanner4');
+            unclickedMinus('signbanner5');
+            document.getElementById('hiddenbanner2').style.display = 'flex';
+            document.getElementById('hiddenbanner1').style.display = 'none';
+            document.getElementById('hiddenbanner3').style.display = 'none';
+            document.getElementById('hiddenbanner4').style.display = 'none';
+            document.getElementById('hiddenbanner5').style.display = 'none';
+        } else if (selectedBanner[i].includes('3')) {
+            clickedBanner = banners[2];
+            clickedColor(clickedBanner);
+            clickedMinus('signbanner3');
+            unclickedMinus('signbanner1');
+            unclickedMinus('signbanner2');
+            unclickedMinus('signbanner4');
+            unclickedMinus('signbanner5');
+            document.getElementById('hiddenbanner3').style.display = 'flex';
+            document.getElementById('hiddenbanner1').style.display = 'none';
+            document.getElementById('hiddenbanner2').style.display = 'none';
+            document.getElementById('hiddenbanner4').style.display = 'none';
+            document.getElementById('hiddenbanner5').style.display = 'none';
+        } else if (selectedBanner[i].includes('4')) {
+            clickedBanner = banners[3];
+            clickedColor(clickedBanner);
+            clickedMinus('signbanner4');
+            unclickedMinus('signbanner1');
+            unclickedMinus('signbanner2');
+            unclickedMinus('signbanner3');
+            unclickedMinus('signbanner5');
+            document.getElementById('hiddenbanner4').style.display = 'flex';
+            document.getElementById('hiddenbanner1').style.display = 'none';
+            document.getElementById('hiddenbanner2').style.display = 'none';
+            document.getElementById('hiddenbanner3').style.display = 'none';
+            document.getElementById('hiddenbanner5').style.display = 'none';
+        } else if (selectedBanner[i].includes('5')) {
+            clickedBanner = banners[4];
+            clickedColor(clickedBanner);
+            clickedMinus('signbanner5');
+            unclickedMinus('signbanner1');
+            unclickedMinus('signbanner2');
+            unclickedMinus('signbanner3');
+            unclickedMinus('signbanner4');
+            document.getElementById('hiddenbanner5').style.display = 'flex';
+            document.getElementById('hiddenbanner1').style.display = 'none';
+            document.getElementById('hiddenbanner2').style.display = 'none';
+            document.getElementById('hiddenbanner3').style.display = 'none';
+            document.getElementById('hiddenbanner4').style.display = 'none';
+        }
     };
 };
 
 
 
 // event handler properties
-let eventAssigment = banner => {
-        banner.addEventListener('mousedown', addToClickedBanner);
+let checkOpenedBanner = banner => {
+    banner.addEventListener('mousedown', bannerHideClicked);
 };
+
+let eventAssigment = banner => {
+    banner.addEventListener('mousedown', addToClickedBanner);
+};
+
 
 // loop
 clickedColor(clickedBanner);
 // clickedMinus(clickedBanner);
 
 banners.forEach(eventAssigment);
+
 
