e44c96e1a2afd150ff75f7c4303fc0729d044916
Jonathan Cano <jonathancanofreta@gmail.com>
2024-08-01 15:06:33 +0200

    added switchers

--- portfolio/resources/js/navbarfixed.js
+++ portfolio/resources/js/navbarfixed.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
     const stickyNav = document.querySelector(".nav-bar-hidden-sticky");
+    const upButton = document.querySelector(".goup-switcher");
 
-    window.addEventListener("scroll", () => {
+    const appearElements = () => {
         let scroll = window.scrollY;
         
         if (scroll > 210) {
             stickyNav.classList.add("sticky-navbar");
+            upButton.classList.add("appear-goup-button");
         } else {
             stickyNav.classList.remove("sticky-navbar");
-        }
+            upButton.classList.remove("appear-goup-button");
+        };
+    };
 
-    });
-    
+    window.addEventListener("scroll", appearElements);
+
 });
